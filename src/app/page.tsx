19f9c298f99a7235980a765ef4b949b3e010cb38
psyfer <psyferblock.io@gmail.com>
2024-06-26 09:56:12 +0300

    prisma mongodb setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
+"use client";
+
 import { navItems } from "@/data";
 
-export default function HomePage() {
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const Home = () => {
   return (
-    <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
-      <div className="w-full max-w-7xl">
-        {" "}
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero />{" "}
-        <Grid/>
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,22 +1,18 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
-import { env } from "@/env";
+let prisma: PrismaClient;
 
-const psClient = new Client({ url: env.DATABASE_URL });
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(psClient),
-  });
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
-
-export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export default prisma;
